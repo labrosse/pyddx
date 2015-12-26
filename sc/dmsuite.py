5bb31faa52ca3c58c9aa1b84404e294f520a2598
Stéphane Labrosse <stephane.labrosse@ens-lyon.fr>
2015-12-26 10:32:12 +0100

    added orrsom function

--- sc/dmsuite.py
+++ sc/dmsuite.py
@@ -1092,96 +1092,39 @@ def sgrhs():
 def schrod():
     pass
 
-def orrsom(N, R):
+def orrsom(ncheb, rey):
     """
-    Compute the eigenvalues of the Orr-Sommerfeld equation using Chebyshev
-    collocation.
+    Eigenvalues of the Orr-Sommerfeld equation using Chebyshev collocation.
 
     Parameters
     ----------
 
-    N   : int
-          number of grid points
+    ncheb : int, number of grid points
 
-    R   : float
-          Reynolds number
+    rey : float, Reynolds number
 
     Returns
     -------
-    x  : ndarray
-         N x 1 array of Hermite nodes which are zeros of the N-th degree
-         Hermite polynomial, scaled by b
-
-    DM : ndarray
-         M x N x N  array of differentiation matrices
-
-    Notes
-    -----
-    This function returns  M differentiation matrices corresponding to the
-    1st, 2nd, ... M-th derivates on a Hermite grid of N points. The
-    matrices are constructed by differentiating N-th order Hermite
-    interpolants.
-
-    The M-th derivative of the grid function f is obtained by the matrix-
-    vector multiplication
-
-    .. math::
-
-    f^{(m)}_i = D^{(m)}_{ij}f_j
-
-    References
-    ----------
-    ..[1] B. Fornberg, Generation of Finite Difference Formulas on Arbitrarily
-    Spaced Grids, Mathematics of Computation 51, no. 184 (1988): 699-706.
-
-    ..[2] J. A. C. Weidemann and S. C. Reddy, A MATLAB Differentiation Matrix
-    Suite, ACM Transactions on Mathematical Software, 26, (2000) : 465-519
-
-    ..[3] R. Baltensperger and M. R. Trummer, Spectral Differencing With A
-    Twist, SIAM Journal on Scientific Computing 24, (2002) : 1465-1487
-
-    Examples
-    --------
-
-    The derivatives of functions can be obtained by multiplying the vector of
-    function values by the differentiation matrix. The N-point Chebyshev
-    approximation of the first two derivatives of y = f(x) can be obtained
-    as
-
-    >>> N = 32; M = 2; pi = np.pi
-    >>> from pyddx.sc import dmsuite as dms
-    >>> x, D = dms.chebdif(N, M)        # first two derivatives
-    >>> D1 = D[0,:,:]                   # first derivative
-    >>> D2 = D[1,:,:]                   # second derivative
-    >>> y = np.sin(2*pi*x)              # function at Chebyshev nodes
-    >>> plot(x, y, 'r', x, D1.dot(y), 'g', x, D2.dot(y), 'b')
-    >>> xlabel('$x$'), ylabel('$y$, $y^{\prime}$, $y^{\prime\prime}$')
-    >>> legend(('$y$', '$y^{\prime}$', '$y^{\prime\prime}$'), loc='upper left')
+    meig : Eigenvalue with largest real part  
     """
+    from scipy import linalg
 
+    # Compute second derivative
+    xxt, ddm = chebdif(ncheb+2, 2, outputx=True)
+    # Enforce Dirichlet BCs
+    dd2 = ddm[1, 1:ncheb+1, 1:ncheb+1]
+    # Compute fourth derivative
+    xxt, dd4 = cheb4c(ncheb+2, outputx=True)
+    # identity matrix
+    ieye = np.eye(dd4.shape[0])
 
-    #i = np.sqrt(-1);
-    #
-    ## compute Chebyshev differentiation matrices
-    #[x,DM] = chebdif(N+2,2);
-    #
-    ## extract the second derivative matrix and enforce Dirichlet bcs
-    #D2 = DM[1, 1:N, 1:N]
-    #
-    #
-    #
-    #    D2 = DM(2:N+1,2:N+1);                    % Enforce Dirichlet BCs
-    #
-    #[x,D4] = cheb4c(N+2);                          % Compute fourth derivative
-    #     I = eye(size(D4));                        % Identity matrix
-    #
-    #A = (D4-2*D2+I)/R-2*i*I-i*diag(1-x.^2)*(D2-I); % Set up A and B matrices
-    #B = D2-I;
-    #
-    #e = eig(A,B);                                  % Compute eigenvalues
-    #
-    #[m,l] = max(real(e));                          % Find eigenvalue of largest
-    #disp('Eigenvalue with largest real part = ')   % real part
-    #disp(e(l))
-    #
+    # setup A and B matrices
+    amat = (dd4-2*dd2+ieye)/rey-2*1j*ieye-1j*np.dot(np.diag(1-xxt**2), (dd2-ieye))
+    bmat = dd2-ieye
+    # Compute eigenvalues
+    eigv = linalg.eig(amat, bmat, right=False)
+    # Find eigenvalue of largest real part
+#    meig = max(np.real(eigv))
+    leig = np.argmax(np.real(eigv))
+    return eigv[leig]
 
